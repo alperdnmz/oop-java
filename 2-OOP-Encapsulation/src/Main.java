9127edf40e8a50cdb73cdb28c9d3adf88038970e
Alper Dönmez <73609222+alperdnmz@users.noreply.github.com>
2024-07-01 14:33:49 +0300

    OOP-2 encapsulation added

--- 2-OOP-Encapsulation/src/Main.java
+++ 2-OOP-Encapsulation/src/Main.java
@@ -1,7 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Cat cat1 = new Cat();
 
+        cat1.setAge(2);
+
+        System.out.println(cat1.getAge());
     }
 }
