2d245288bc7654b73cfcca02b076089d1a405d90
Alper Dönmez <73609222+alperdnmz@users.noreply.github.com>
2024-07-01 14:57:14 +0300

    OOP-3 inheritance added

---
+++ 3-OOP-Inheritance/src/Cat.java
@@ -0,0 +1,22 @@
+class Cat {
+    public String name;
+    public String color;
+    public int age;
+    public int hungry;
+
+    void eatFood(int foodVal){
+        this.hungry += foodVal;
+    }
+}
+
+class VanCat extends Cat {
+
+}
+
+class TekirCat extends Cat {
+
+}
+
+class SarmanCat extends Cat {
+
+}
--- 3-OOP-Inheritance/src/Main.java
+++ 3-OOP-Inheritance/src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        VanCat cat1 = new VanCat();
+        TekirCat cat2 = new TekirCat();
+        SarmanCat cat3 = new SarmanCat();
+
+        cat1.name = "Poseidon";
+        cat1.color = "White";
+        cat1.age = 2;
+        cat1.hungry = 6;
+
+        cat2.name = "Hades";
+        cat2.color = "Brown";
+        cat2.age = 5;
+        cat2.hungry = 2;
+
+        cat3.name = "Zeus";
+        cat3.color = "Yellow";
+        cat3.age = 1;
+        cat3.hungry = 3;
+
+        System.out.println(cat1.name + " | " + cat1.color + " | " + cat1.age + " | " + cat1.hungry);
+        System.out.println(cat2.name + " | " + cat2.color + " | " + cat2.age + " | " + cat2.hungry);
+        System.out.println(cat3.name + " | " + cat3.color + " | " + cat3.age + " | " + cat3.hungry);
     }
 }
